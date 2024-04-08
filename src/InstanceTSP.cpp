3c5f01e7afc2c141becefcdb3e9118291e143507
GaruXer <simon@deweerdt.net>
2024-04-08 16:35:32 +0200

    feat: ajout de la classe InstanceTSP

---
+++ src/InstanceTSP.cpp
@@ -0,0 +1,32 @@
+#include "InstanceTSP.h"
+
+#include <fstream>
+
+InstanceTSP::InstanceTSP(const std::string& filename) {
+    lireInstance(filename);
+}
+
+void InstanceTSP::lireInstance(const std::string& filename) {
+    std::ifstream file(filename);
+
+    int n;
+    file >> n;
+
+    villes.resize(n);
+
+    for (int i = 0; i < n; i++) {
+        Ville v;
+        file >> v.nom >> v.latitude >> v.longitude;
+        villes[i] = v;
+    }
+}
+
+void InstanceTSP::afficherInstance() {
+    for (auto & ville : villes) {
+        std::cout << ville.nom << " ";
+    }
+}
+
+std::vector<Ville> InstanceTSP::getVilles() {
+    return villes;
+}
---
+++ src/InstanceTSP.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+typedef struct v {
+    float latitude;
+    float longitude;
+    std::string nom;
+} Ville;
+
+class InstanceTSP {
+public:
+    InstanceTSP(const std::string& filename);
+    void lireInstance(const std::string& filename);
+    void afficherInstance();
+
+    std::vector<Ville> getVilles();
+protected:
+    std::vector<Ville> villes;
+};
